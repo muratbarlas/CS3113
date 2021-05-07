67f50d1f313fffb676f7914220588b43add13678
muratbarlas <mlb771@nyu.edu>
2021-05-06 23:17:01 -0400

    name change

--- project 6/SDLProject/LevelMenu.cpp
+++ project 6/SDLProject/LevelMenu.cpp
@@ -21,10 +21,6 @@ void LevelMenu::Update(float deltaTime) {
 }
 
 void LevelMenu::Render(ShaderProgram *program) {
-    Util::DrawText(program,  fontTextureID5, "GEORGE ON THE RUN", 1.1f, -0.55f, glm:: vec3(0.6, -2.5,0.0f));
+    Util::DrawText(program,  fontTextureID5, "SPACE MAZE", 1.1f, -0.55f, glm:: vec3(0.6, -2.5,0.0f));
     Util::DrawText(program,  fontTextureID5, "Press Enter to start", 0.8f, -0.45f, glm:: vec3(1.9, -3.5,0.0f));
-    Util::DrawText(program,  fontTextureID5, "Don't fall or hit the AIs!", 0.6f, -0.35f, glm:: vec3(2.2, -5.5,0.0f));
-    Util::DrawText(program,  fontTextureID5, "(it's ok to jump on top of them)", 0.6f, -0.35f, glm:: vec3(1.5, -6.5,0.0f));
-    
-    
 }
